d237e41fbe983423d30063e699ba073dbe1da9ac
mdimran1409036 <mdimran1409036@gmail.com>
2021-01-20 02:42:17 +0600

    Firebase config file added

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfwmNQ_UEraDdx11NNcAWRHlbCdlvk-ls",
+  authDomain: "dojo-blog-90616.firebaseapp.com",
+  databaseURL: "https://dojo-blog-90616-default-rtdb.firebaseio.com",
+  projectId: "dojo-blog-90616",
+  storageBucket: "dojo-blog-90616.appspot.com",
+  messagingSenderId: "517108458849",
+  appId: "1:517108458849:web:851461cf93a92b16e9d120",
+  measurementId: "G-ZSQ6KEHH50"
+};
+
+
+//init firebase
+
+firebase.initializeApp(FirebaseConfig)
+
+//init firestore service
+
+const projectFirestore = firebase.firestore()
+
+export {projectFirestore}
